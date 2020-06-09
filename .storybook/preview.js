f9e91cd7af7d6976516e4472de57864e52da49c8
Daniele Bertella <daniele.bertella@gmail.com>
2020-06-10 00:36:27 +0100

    Add theme ui config via and add on in storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import { ThemeProvider } from "theme-ui";
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "@react-theming/storybook-addon";
+
+import { theme } from "./theme";
+
+// pass ThemeProvider and array of your themes to decorator
+addDecorator(withThemes(ThemeProvider, [theme]));
--- stories/1-Button.stories.tsx
+++ stories/1-Button.stories.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "theme-ui";
 
 export default {
-  title: 'Button',
+  title: "Button",
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => (
+  <Button sx={{ color: "primary", bg: 'text' }} onClick={action("clicked")}>
+    Hello Button
+  </Button>
+);
 
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button onClick={action("clicked")}>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
