1fbe957c99b5424d16ff8ae2e289c2d96d8e086b
alberto <alberto.massa@ymail.com>
2020-06-12 00:05:48 +0200

    improvement(storybook): use our exported theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'theme-ui'
 import { addDecorator } from '@storybook/react'
 import { withThemes } from '@react-theming/storybook-addon'
 
-import { theme } from './theme'
+import { theme } from '../src/theme'
 
 // pass ThemeProvider and array of your themes to decorator
 addDecorator(withThemes(ThemeProvider, [theme]))
